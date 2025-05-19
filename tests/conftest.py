c0cdad4dde592f7f77d4207f51b3b458e453be34
Akshath Mangudi <akshathmangudi@gmail.com>
2025-05-20 00:33:48 +0530

    fixing build issues

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,5 +6,5 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__))))
 
 # Import the necessary modules to ensure registry is properly initialized
-from registry.model_registry import MODEL_REGISTRY, register_model, get_model
-import models
+from refrakt_core.registry.model_registry import MODEL_REGISTRY, register_model, get_model
+import refrakt_core.models
