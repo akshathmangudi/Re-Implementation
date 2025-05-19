b61d2ddb0c3cc2f42c7f9d38d12889af60cf43b5
Akshath Mangudi <akshathmangudi@gmail.com>
2025-05-20 00:54:52 +0530

    fixing build issues

--- src/refrakt_core/models/templates/__init__.py
+++ src/refrakt_core/models/templates/__init__.py
@@ -1,4 +1,4 @@
 from refrakt_core.models.templates.base import BaseModel
-from refrakt_core.models import BaseClassifier, BaseAutoEncoder, BaseContrastiveModel, BaseGAN
+from .models import BaseClassifier, BaseAutoEncoder, BaseContrastiveModel, BaseGAN
 
 __all__ = ["BaseModel", "BaseClassifier", "BaseAutoEncoder", "BaseContrastiveModel", "BaseGAN"]
