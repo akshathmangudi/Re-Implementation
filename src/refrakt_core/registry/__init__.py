2edec007748d1736869f818671a9e97978ae14b1
Akshath Mangudi <akshathmangudi@gmail.com>
2025-05-29 14:35:29 +0530

    loss and trainer registry added

--- src/refrakt_core/registry/__init__.py
+++ src/refrakt_core/registry/__init__.py
@@ -1,3 +1,9 @@
 from refrakt_core.registry.model_registry import register_model, get_model, MODEL_REGISTRY
+from refrakt_core.registry.loss_registry import register_loss, get_loss, LOSS_REGISTRY
+from refrakt_core.registry.trainer_registry import register_trainer, get_trainer, TRAINER_REGISTRY
 
-__all__ = ["register_model", "get_model", "MODEL_REGISTRY"]
+__all__ = [
+    "register_model", "get_model", "MODEL_REGISTRY",
+    "register_loss", "get_loss", "LOSS_REGISTRY",
+    "register_trainer", "get_trainer", "TRAINER_REGISTRY"
+]
---
+++ src/refrakt_core/trainer/__init__.py
@@ -0,0 +1,13 @@
+import os
+import importlib
+
+__all__ = []
+
+def auto_import_trainers():
+    trainer_dir = os.path.dirname(__file__)
+    for filename in os.listdir(trainer_dir):
+        if filename.endswith(".py") and filename != "__init__.py":
+            module_name = f"{__name__}.{filename[:-3]}"
+            importlib.import_module(module_name)
+
+auto_import_trainers()
